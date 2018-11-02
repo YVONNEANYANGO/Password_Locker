1581f0ffdef821a8eadcaa81df4cc87b7f3bebd9
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-02 14:42:52 +0300

    add a set up method to allow for the instructions which will be passed to be executed before each tests,add testCase method called self.assertEqual to check for an expected result

--- credential_test.py
+++ credential_test.py
@@ -9,3 +9,25 @@ class TestCredential(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     """
+
+
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_contact = Contact("Facebook","Yvonnah Bonswuh","ivonnahbonswuh@gmail.com","ivy1996") # create credential object
+
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+
+        self.assertEqual(self.new_credential.account_name,"Facebook")
+        self.assertEqual(self.new_credential.user_name,"Yvonnah Bonswuh")
+        self.assertEqual(self.new_credential.email_address,"ivonnahbonswuh@gmail.com")
+        self.assertEqual(self.new_credential.password,"ivy1996")
+
+
+if __name__ == '__main__':
+    unittest.main()
