8c278855d265ddab71f6ba705e20b88ef2bba932
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-02 12:42:24 +0300

    add class credentials that contails descriptions of the users

---
+++ password.py
@@ -0,0 +1,13 @@
+Class Credential:
+"""
+Class that generates new instances of user credentials
+"""
+
+def __init__(self,account_name,user_name,email_address,password)
+    
+    Args:
+        account_name: New credential account name.
+        user_name: New credential user name.
+        email_address: New credential email address.
+        password: New credential password.
+"""
