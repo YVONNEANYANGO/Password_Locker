9d0cdfcf92c1e06b7b9f27a9bd459c4b87b93429
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-02 13:16:11 +0300

    add python test framework..unittest and the imported credential class,a TestCredential class that inherits from unittest.TestCase

---
+++ credential_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module 
+from credential import Credential # Importing the credential class
+
+class TestCredential(unittest.TestCase):
+
+    """
+    Test class defines test cases for the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
