f7f177d5b125149409b44a346773f6349a804d48
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-02 12:51:50 +0300

    add four instance variables to take up our initial values for our new contact

--- password.py
+++ password.py
@@ -3,11 +3,19 @@ Class Credential:
 Class that generates new instances of user credentials
 """
 
-def __init__(self,account_name,user_name,email_address,password)
+    def __init__(self,account_name,user_name,email_address,password)
+
+    """
+    __init__ method that helps us define properties for our objects
     
-    Args:
-        account_name: New credential account name.
-        user_name: New credential user name.
-        email_address: New credential email address.
-        password: New credential password.
-"""
+        Args:
+            account_name: New credential account name.
+            user_name: New credential user name.
+            email_address: New credential email address.
+            password: New credential password.
+      """
+
+      self.account_name = account_name
+      self.user_name = user_name
+      self.email_address = email_address
+      self.password = password
